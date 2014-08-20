3933e95bc2a891f3474d6a823f1d757ab471ff03
nlinton <nlinton@47a60c44-a02a-4305-9b0b-3482a82a218c>
2014-08-20 16:31:44 +0000

    Added a player branch that's merging Nick and Corey's player code with Clinton's camera code and test scene - Nick and Corey

---
+++ branches/player branch/Assets/Scripts/PlayerBase.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum playerClass
+{
+	WARRIOR, 
+	SORCERER, 
+	ROGUE, 
+	ADVENTURER
+};
+
+public class PlayerBase : MonoBehaviour 
+{
+	private playerClass pClass;
+
+	private int health = 100;
+	private int maxHealth;
+
+	private float moveSpeed = 6.0f;
+	private float visibility = 1.0f;
+
+	private bool canJump = true;
+	private float jumpForce = 0.25f;
+	private float verticalVelocity = 0.0f;
+
+	// controls
+	public string moveAxisX;
+	public string moveAxisZ;
+	public KeyCode jumpKey;
+
+	private GameObject item;
+
+	private CharacterController charControl;
+
+	void Start()
+	{
+		maxHealth = health;
+		charControl = GetComponent<CharacterController>();
+	}
+
+	void FixedUpdate()
+	{
+		// Get the horizontal movement from the joystick input and scale it with moveSpeed
+		Vector3 moveVec = new Vector3(Input.GetAxis(moveAxisX), 0.0f, Input.GetAxis(moveAxisZ));
+		moveVec *= moveSpeed * Time.deltaTime;
+
+		// Handle jumping and add it to the movement vector
+		if (canJump && Input.GetKeyDown(jumpKey))
+		{
+			verticalVelocity = jumpForce;
+			canJump = false;
+		}
+		else if (charControl.isGrounded)
+		{
+			verticalVelocity = 0.0f;
+			canJump = true;
+		}
+		else
+		{
+			verticalVelocity += Physics.gravity.y * 0.1f * Time.deltaTime;
+		}
+
+		moveVec = new Vector3(moveVec.x, verticalVelocity, moveVec.z);
+
+		charControl.Move(moveVec);
+	}
+}
---
+++ branches/player branch/Assets/Scripts/p2Light.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class p2Light : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
