81c5c22943ca06835f266ffdbcc586a3067eadab
cschuette <cschuette@47a60c44-a02a-4305-9b0b-3482a82a218c>
2014-08-20 18:27:19 +0000

    added the object branch, made a base project to write the scripts in to move over for later. CS, JB, VV, JJ

---
+++ branches/objectBranch/base/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public float speed;
+
+	private GameObject item;
+	private float health;
+	public Potion p;
+
+	void Update()
+	{
+		Debug.Log ("Health: " + health.ToString());
+		if (Input.GetKeyDown ("space")) 
+		{
+			if(item)
+			{
+				p = item.GetComponent("Potion") as Potion;
+				health += p.potionValue;
+				Destroy(item);
+			}
+		}
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 move = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce(move * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Potion") 
+		{
+			item = other.gameObject;
+			p = item.GetComponent("Potion") as Potion;
+			other.gameObject.SetActive (false);
+			Debug.Log ("Item attached: " + item.name);
+		} 
+	}
+
+
+}
---
+++ branches/objectBranch/base/Assets/Scripts/Potion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Potion : MonoBehaviour {
+
+	public float potionValue = 20.0f;
+
+	void Update ()
+	{
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+
+	public void UseItem()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		player.SendMessage ("addHealth", potionValue);
+	}
+}
