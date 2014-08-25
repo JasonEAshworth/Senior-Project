b5a7b543df28187e4e0fa0098048b3470e45c1a4
nlinton <nlinton@47a60c44-a02a-4305-9b0b-3482a82a218c>
2014-08-25 19:57:10 +0000

    Started work on the basics of a map manager, setting up appropriate data structures that we will need. - Nick Linton

---
+++ branches/mapBranch/Assets/MapManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum RoomType
+{
+	HORDE,
+	PUZZLE,
+	BOSS,
+	TREASURE,
+	SQUIRREL
+}
+
+public class RoomGraph
+{
+	public List<RoomNode> rooms;
+
+	public RoomGraph()
+	{
+		rooms = new List<RoomNode>();
+	}
+
+	public void addRoom(string name)
+	{
+		RoomNode room = new RoomNode(name);
+	}
+}
+
+public class RoomNode
+{
+	public string name;
+	public RoomType type;
+	public RoomNode north;
+	public RoomNode east;
+	public RoomNode south;
+	public RoomNode west;
+
+	public RoomNode(string n)
+	{
+		name = n;
+		// room type
+		if (n.Contains("_H_"))
+		{
+			type = RoomType.HORDE;
+		}
+		else if (n.Contains("_P_"))
+		{
+			type = RoomType.PUZZLE;
+		}
+		else if (n.Contains("_B_"))
+		{
+			type = RoomType.BOSS;
+		}
+		else if (n.Contains("_T_"))
+		{
+			type = RoomType.TREASURE;
+		}
+		else
+		{
+			type = RoomType.SQUIRREL;
+		}
+	}
+}
+
+public class MapManager : MonoBehaviour 
+{
+	private RoomGraph map;
+	public string[] roomsToLoad = new string[3];
+
+	void Start()
+	{
+		map = new RoomGraph();
+		for (int i = 0; i < roomsToLoad.Length; i++)
+		{
+			map.addRoom(roomsToLoad[i]);
+		}
+	}
+}
