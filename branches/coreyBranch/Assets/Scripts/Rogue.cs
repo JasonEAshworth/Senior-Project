10e29e2969e42467ca5e2195ee4a4be8101439d8
ccooper <ccooper@47a60c44-a02a-4305-9b0b-3482a82a218c>
2014-08-27 19:02:28 +0000

    added the corey branch, soon to be the trunk

---
+++ branches/coreyBranch/Assets/Scripts/Rogue.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rogue : PlayerBase
+{
+	void start()
+	{
+		int health = 100;
+		int maxHealth = health;
+		float visibility = 1.0f;
+		float moveSpeed = 6.0f;
+	}
+
+	public override void basicAttack()
+	{
+		if(Input.GetKeyDown (basicAttackKey)){
+			//Check enemy facing
+			Debug.Log ("warrior basic attack");
+		}
+	}
+	
+	public override void specialAttack()
+	{
+		if(Input.GetKeyDown (specialAttackKey))
+		{
+			//Envenom
+			Debug.Log ("warrior special attack");
+		}
+	}
+	
+	public override void classAbility()
+	{
+		if(Input.GetKeyDown (classAbilityKey))
+		{
+			visibility = 0.0f;
+			Debug.Log ("warrior class ability");
+		}
+	}
+}
