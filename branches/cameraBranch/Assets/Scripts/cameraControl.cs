e881f678e20bfafa5a10be3cb5e9b210899491b8
jjackson <jjackson@47a60c44-a02a-4305-9b0b-3482a82a218c>
2014-09-03 23:14:43 +0000

    Another update to the Camera class. I went and relooked at how we went about positioning the camera, and came up with a different solution based that I feel is a bit easier to understand, and to update. The current method has a range that the X, Y, Z, and Points of interest can go. The position of the players in the world are calculated every frame. As they move away from one another, the camera's Z and Y pull back to keep all players in frame. The x pans when the players move towards either side of the map. I've elected to pan it towards the center of the map. While this does force us to complete a solution to the 'When players are behind a wall' issue sooner, I feel this is the only way to keep enough information on screen to let the players know what's going on. Lastly, I created an offset for the point of interest of the camera which also shifts based on how far away they are from the center of the screen on the Z and X axis. This helps keep the camera focused on the game and avoids having too much dead space when the players are in the corners of the map.
    There are some major limitations with the current version. The biggest is that I couldn't find a way to keep all players on screen that kept the players are a reasonable size. As such, I simply elected to complete the original goal; keep them all on screen. We will need to debate how to deal with the size issue soon however. Either we'll need to shrink the game world so there's less ground that could separate them, or we need to adjust the ratios of the rooms. Currently they're square. If we were to shrink their z axis slightly, and make them in roughly 16:9, I think we'd also get around the problem. Will be talking to folks about this ASAP. 
    
    -James Jackson

--- branches/cameraBranch/Assets/Scripts/cameraControl.cs
+++ branches/cameraBranch/Assets/Scripts/cameraControl.cs
@@ -6,91 +6,127 @@ using System;
 
 public class cameraControl : MonoBehaviour
 {
-		private bool isOrthographic;
-		public GameObject[] targets;
-		public float currentDistance;
-		public float largestDistance;
-		public Camera theCamera;
-		public float height = 5.0f;
-		public float avgDistance;
-		public float distance = 5.0f;                    // Default Distance 
-		public float speed = 1;
-		public float offset;
-		public float roomLength = 48f;
+    private bool isOrthographic;
+    public GameObject[] targets;
+    public GameObject avgBox;
+    public float currentDistance;
+    public float largestDistance;
+    public Camera theCamera;
+    public float height = 5.0f;
+    public Vector3 avgDistance;
+    public float distance = 5.0f;                    // Default Distance 
+    public float speed = 1;
+    public float offset;
+    public float roomLength = 48f;
+    private float roomDiagonal;
+    public float xRatio = 0f;
+    public float yzRatio = 0f;
+    public float offZRatio = 0f;
+    public float offXRatio = 0f;
+    public float[] xClamp = {4,-4};
+    public float[] yClamp = {2,37};
+    public float[] zClamp = {0,26};
+    public float[] offsetClamp = {0, 7, 0, 0};
 
-		public void Start ()
-		{
-			Console.WriteLine ("START");
-			targets = GameObject.FindGameObjectsWithTag ("Player");
-			if (theCamera) {
-				isOrthographic = theCamera.orthographic;
-			}
-		}
+    public void Start ()
+    {
+        Console.WriteLine ("START");
+        targets = GameObject.FindGameObjectsWithTag ("Player");
+        avgBox = GameObject.FindGameObjectWithTag ("avgBox");
+        // Square diagonal = Sqrt(2) * Length 
+        roomDiagonal = 1.414f * roomLength;
+        if (theCamera) {
+            isOrthographic = theCamera.orthographic;
+        }
+    }
 
-		public void OnGUI ()
-		{
-				GUILayout.Label ("largest distance is = " + largestDistance.ToString ());
-				GUILayout.Label ("height = " + height.ToString ());
-				GUILayout.Label ("number of players = " + targets.Length.ToString ());
-		}
+    public void OnGUI ()
+    {
+        GUILayout.Label ("largest distance is = " + largestDistance.ToString ());
+        GUILayout.Label ("height = " + height.ToString ());
+        GUILayout.Label ("number of players = " + targets.Length.ToString ());
+        GUILayout.Label ("xRatio = " + xRatio.ToString ());
+        GUILayout.Label ("yzRatio = " + yzRatio.ToString ());
+        GUILayout.Label ("offRatio = " + offZRatio.ToString ());
+        GUILayout.Label ("offRatio = " + offXRatio.ToString ());
+        GUILayout.Label ("Average Distance = " + avgDistance.x.ToString () + ", " + avgDistance.y.ToString () + ", " + avgDistance.z.ToString ());
+    }
 
-		public void LateUpdate ()
-		{
-				targets = GameObject.FindGameObjectsWithTag ("Player");
-				if (!GameObject.FindWithTag ("Player")) {
-						return;
-				}
-				Vector3 sum = new Vector3 (0, 0, 0);
-				for (int i = 0; i < targets.Length; i++) {
-						sum += targets [i].transform.position;
-				}
+    public void LateUpdate ()
+    {
+        if (!GameObject.FindWithTag ("Player")) {
+            return;
+        }
+        Vector3 sum = new Vector3 (0, 0, 0);
+        for (int i = 0; i < targets.Length; i++) {
+            sum += targets [i].transform.position;
+        }
+        
+        avgDistance = sum / targets.Length;
+        
+        float largestDifference = returnLargestDifference ();
+        // New code work area
+                
+        xRatio = (avgDistance.x / roomLength) + .5f;
+        float tempX = avgDistance.x + Mathf.Lerp (xClamp [0], xClamp [1], xRatio);
 
-				Vector3 avgDistance = sum / targets.Length;
+        //float yzRatio = largestDistance / roomDiagonal;
+        yzRatio = largestDistance / roomLength;
+        float tempY = Mathf.Lerp (yClamp [0], yClamp [1], yzRatio);
+        float tempZ = avgDistance.z + 5 + Mathf.Lerp (zClamp [0], zClamp [1], yzRatio);
 
-				float largestDifference = returnLargestDifference ();
+        offZRatio = (avgDistance.z / roomLength) + .5f;
+        float lookZOffset = Mathf.Lerp (offsetClamp [0], offsetClamp [1], offZRatio);
+        float lookXOffset = Mathf.Lerp (offsetClamp [2], offsetClamp [3], xRatio);
+                
+        // Old code
+        
+        theCamera.transform.position = new Vector3 (tempX, tempY, tempZ);
+        theCamera.transform.LookAt (new Vector3 ((avgDistance.x + lookXOffset), avgDistance.y, (avgDistance.z + lookZOffset)));
+        avgBox.transform.position = new Vector3 ((avgDistance.x + lookXOffset), avgDistance.y, (avgDistance.z + lookZOffset));
+                
+    }
 
-				distance = Mathf.Lerp (height, largestDifference, 0.5f);
-
-				if (largestDifference < 8) {
-						distance = 8;
-						Vector3 temp = new Vector3 (theCamera.transform.position.x, 10, theCamera.transform.position.z);
-						theCamera.transform.position = temp;
-				}
-
-				if (largestDifference > 8) {
-						distance = 8;
-				}
-
-				//float tempX = avgDistance.x - 2;
-				float tempZ = avgDistance.z + distance + 0.5f * (largestDifference);
-				float tempY = distance;
-			
-	
-		
-				// New code work area
-				
-				float xRatio = avgDistance.x / roomLength;
-				float tempX = avgDistance.x + Mathf.Lerp (4, -4, xRatio);
-				
-				// Old code
-		
-				theCamera.transform.position = new Vector3 (tempX, tempY, tempZ);
-				theCamera.transform.LookAt (avgDistance);
-				
-		}
-
-		public float returnLargestDifference ()
-		{
-				currentDistance = 0.0f;
-				largestDistance = 0.0f;
-				for (int i = 0; i < targets.Length; i++) {
-						for (int j = 0; j < targets.Length; j++) {
-								currentDistance = Vector3.Distance (targets [i].transform.position, targets [j].transform.position);
-								if (currentDistance > largestDistance) {
-										largestDistance = currentDistance;
-								}
-						}
-				}
-				return largestDistance;
-		}
+    public float returnLargestDifference ()
+    {
+        currentDistance = 0.0f;
+        largestDistance = 0.0f;
+        for (int i = 0; i < targets.Length; i++) {
+            for (int j = 0; j < targets.Length; j++) {
+                currentDistance = Vector3.Distance (targets [i].transform.position, targets [j].transform.position);
+                if (currentDistance > largestDistance) {
+                    largestDistance = currentDistance;
+                }
+            }
+        }
+        return largestDistance;
+    }
 }
+
+
+/* OLD CODE
+ * targets = GameObject.FindGameObjectsWithTag ("Player");
+        if (!GameObject.FindWithTag ("Player")) {
+            return;
+        }
+        Vector3 sum = new Vector3 (0, 0, 0);
+        for (int i = 0; i < targets.Length; i++) {
+            sum += targets [i].transform.position;
+        }
+
+        avgDistance = sum / targets.Length;
+
+        float largestDifference = returnLargestDifference ();
+
+        distance = Mathf.Lerp (height, largestDifference, 0.5f);
+
+        if (largestDifference < 8) {
+            distance = 8;
+            Vector3 temp = new Vector3 (theCamera.transform.position.x, 10, theCamera.transform.position.z);
+            theCamera.transform.position = temp;
+        }
+
+        if (largestDifference > 8) {
+            distance = 8;
+        }
+        */
