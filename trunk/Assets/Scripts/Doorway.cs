a613aaf53260af2d494f7a72c7bc5f05402ddbdd
nlinton <nlinton@47a60c44-a02a-4305-9b0b-3482a82a218c>
2014-09-08 16:10:25 +0000

    Added the doorway script because it wasn't added earlier

---
+++ trunk/Assets/Scripts/Doorway.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Doorway : MonoBehaviour 
+{
+	private MapManager mapMan;
+
+	public Direction doorwayDirection;
+	private bool transitioning = false;
+	private float transitionTimer = 1.5f;
+
+	void Start()
+	{
+		mapMan = GameObject.Find("MapManager").GetComponent<MapManager>();
+	}
+
+	void Update()
+	{
+		if (transitioning && transitionTimer > 0.0f)
+		{
+			transitionTimer -= Time.deltaTime;
+			if (transitionTimer <= 0.0f)
+			{
+				mapMan.moveToNewRoom(doorwayDirection);
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision c)
+	{
+		if (!transitioning && c.gameObject.tag == "Player")
+		{
+			transitioning = true;
+			c.gameObject.SendMessage("disableControl");
+		}
+	}
+}
