1089c62665c5c26634258af24147072280a8071a
jjohnson <jjohnson@47a60c44-a02a-4305-9b0b-3482a82a218c>
2014-09-08 17:57:22 +0000

    Added a Scene change script to GUI. when you click a button the scene changes. also add room grid to the documentation folder.

---
+++ branches/guiBranch/Assets/Change.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Change : MonoBehaviour {
+
+
+	// Update is called once per frame
+	public void ChangeToScene(int scene) {
+		Application.LoadLevel (scene);
+	}
+}
