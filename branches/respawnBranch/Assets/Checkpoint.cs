e5ce1e424f1feec4ffe12380722cf200756f4c84
mstrickland <mstrickland@47a60c44-a02a-4305-9b0b-3482a82a218c>
2014-09-08 16:13:47 +0000

    Mark S.: Added checkpoint/respawn code. Can be found in branches/respawnBranch There are gameObjects called Spawner and Checkpoint[insert number here], and when the player collides with a  Checkpoint, it executes Checkpoint.cs, which changes Spawner's position to that of the checkpoint the player collided with. For the sake of testing, another gameObject, Respawn was created, which executes Respawn.cs when the player collides with it, changing the player's position to that of Spawner's.

---
+++ branches/respawnBranch/Assets/Checkpoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+	public Transform checkpoint;
+	
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Player")
+		{
+			checkpoint.position = transform.position;
+		}
+	}
+}
---
+++ branches/respawnBranch/Assets/Scripts/Respawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+
+	public Transform spawns;
+
+	GameObject Player;
+
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Player")
+		{
+			Player=col.gameObject;
+			Player.transform.position = spawns.position;
+		}
+	}
+}
