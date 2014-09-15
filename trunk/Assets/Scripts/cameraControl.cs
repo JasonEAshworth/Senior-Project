d99ae41e645d417fde4a84a3238cb045039288df
jjackson <jjackson@47a60c44-a02a-4305-9b0b-3482a82a218c>
2014-09-15 17:30:26 +0000

    Taking our working, basic camera and putting it into the trunk. This should, hopefully, give the team a better idea what the final camera will be like, and will let them start coding around this style of camera.
    - James

--- trunk/Assets/Scripts/cameraControl.cs
+++ trunk/Assets/Scripts/cameraControl.cs
@@ -6,79 +6,154 @@ using System;
 
 public class cameraControl : MonoBehaviour
 {
-		private bool isOrthographic;
-		public GameObject[] targets;
-		public float currentDistance;
-		public float largestDistance;
-		public Camera theCamera;
-		public float height = 5.0f;
-		public float avgDistance;
-		public float distance = 5.0f;                    // Default Distance 
-		public float speed = 1;
-		public float offset;
+    private bool isOrthographic;
+    public GameObject[] targets;
+    //public GameObject avgBox;
+    public float currentDistance;
+    public float largestDistance;
+    public Camera theCamera;
+    public float height = 5.0f;
+    public Vector3 avgDistance;
+    public float distance = 5.0f;                    // Default Distance 
+    public float speed = 1;
+    public float offset;
+    public float roomLength = 48f;
+    private float roomDiagonal;
+    public float xRatio = 0f;
+    public float yzRatio = 0f;
+    public float offZRatio = 0f;
+    public float offXRatio = 0f;
+    public float[] xClamp = {4,-4};
+    public float[] yClamp = {2,37};
+    public float[] zClamp = {0,26};
+    public float[] offsetClamp = {0, 7, 0, 0};
+	public bool dubugText = false;
 
-		public void Start ()
-		{
-			Console.WriteLine ("START");
-			targets = GameObject.FindGameObjectsWithTag ("Player");
-			if (theCamera) {
-				isOrthographic = theCamera.orthographic;
-			}
-		}
+    // Starts the camera, gets the player targest, and gets the diagonal distance of the room
+    public void Start ()
+    {
+        Console.WriteLine ("START");
+        targets = GameObject.FindGameObjectsWithTag ("Player");
+        //avgBox = GameObject.FindGameObjectWithTag ("avgBox");
+        // Square diagonal = Sqrt(2) * Length 
+        roomDiagonal = 1.414f * roomLength;
+        if (theCamera) {
+            isOrthographic = theCamera.orthographic;
+        }
+    }
 
-		public void OnGUI ()
-		{
-				GUILayout.Label ("largest distance is = " + largestDistance.ToString ());
-				GUILayout.Label ("height = " + height.ToString ());
-				GUILayout.Label ("number of players = " + targets.Length.ToString ());
-		}
+    // All the GUI debug info
+    public void OnGUI ()
+    {
+        GUILayout.Label ("largest distance is = " + largestDistance.ToString ());
+        GUILayout.Label ("height = " + height.ToString ());
+        GUILayout.Label ("number of players = " + targets.Length.ToString ());
+        GUILayout.Label ("xRatio = " + xRatio.ToString ());
+        GUILayout.Label ("yzRatio = " + yzRatio.ToString ());
+        GUILayout.Label ("offRatio = " + offZRatio.ToString ());
+        GUILayout.Label ("offRatio = " + offXRatio.ToString ());
+		GUILayout.Label ("Average Distance = " + avgDistance.x.ToString () + ", " + avgDistance.y.ToString () + ", " + avgDistance.z.ToString ());
+	}
 
-		public void LateUpdate ()
-		{
-				targets = GameObject.FindGameObjectsWithTag ("Player");
-				if (!GameObject.FindWithTag ("Player")) {
-						return;
-				}
-				Vector3 sum = new Vector3 (0, 0, 0);
-				for (int i = 0; i < targets.Length; i++) {
-						sum += targets [i].transform.position;
-				}
+    public void LateUpdate ()
+    {
+        // If we can't find a player, we return without altering the camera's position
+        if (!GameObject.FindWithTag ("Player")) {
+            return;
+        }
 
-				Vector3 avgDistance = sum / targets.Length;
+        // We sum the positions of all of the players, and from there we find the mid point between all of them
+        Vector3 sum = new Vector3 (0, 0, 0);
+        for (int i = 0; i < targets.Length; i++) {
+            sum += targets [i].transform.position;
+        }
+        avgDistance = sum / targets.Length;
 
-				float largestDifference = returnLargestDifference ();
+        // Next, we find what the biggest difference in distance between any two characters is
+        float largestDifference = returnLargestDifference ();
+                
+        // The camera is done via clamping. The positions of the player weights how big of an offset
+        // is used. This allows us to zoom in and out, and shift the camera to the left and right
+        // to keep the room mostly in room. 
 
-				distance = Mathf.Lerp (height, largestDifference, 0.5f);
+        // The xRatio of the room is what decides how far to the left or right the camera will shift
+        // It is the current position of the players center divided by the rooms length
+        xRatio = (avgDistance.x / roomLength) + .5f;
+        float tempX = avgDistance.x + Mathf.Lerp (xClamp [0], xClamp [1], xRatio);
 
-				if (largestDifference < 8) {
-						distance = 8;
-						Vector3 temp = new Vector3 (theCamera.transform.position.x, 10, theCamera.transform.position.z);
-						theCamera.transform.position = temp;
-				}
+        // The yzRatio is the largest difference between two characters divided by the length. This is
+        // used to decide the position of the Y, and Z of the camera depending on exactly where the players are
+        yzRatio = largestDistance / roomLength;
+        float tempY = Mathf.Lerp (yClamp [0], yClamp [1], yzRatio);
+        float tempZ = avgDistance.z + 5 + Mathf.Lerp (zClamp [0], zClamp [1], yzRatio);
 
-				if (largestDifference > 8) {
-						distance = 8;
-				}
+        // The offZRatio decides the exact position where the camera looks at. This lets the camera not always look
+        // dead center, which is sometimes needed to finetune the exacts of the camera
+        offZRatio = (avgDistance.z / roomLength) + .5f;
+        float lookZOffset = Mathf.Lerp (offsetClamp [0], offsetClamp [1], offZRatio);
+        float lookXOffset = Mathf.Lerp (offsetClamp [2], offsetClamp [3], xRatio);
 
-				float tempX = avgDistance.x - 2;
-				float tempZ = avgDistance.z + distance + 0.5f * (largestDifference);
-				float tempY = distance;
-				theCamera.transform.position = new Vector3 (tempX, tempY, tempZ);
-				theCamera.transform.LookAt (avgDistance);
-		}
+        // The camera is set to its new position, pointed to the point to look at. 
+        theCamera.transform.position = new Vector3 (tempX, tempY, tempZ);
+        theCamera.transform.LookAt(new Vector3 ((avgDistance.x + lookXOffset), avgDistance.y, (avgDistance.z + lookZOffset)));
 
-		public float returnLargestDifference ()
-		{
-				currentDistance = 0.0f;
-				largestDistance = 0.0f;
-				for (int i = 0; i < targets.Length; i++) {
-						for (int j = 0; j < targets.Length; j++) {
-								currentDistance = Vector3.Distance (targets [i].transform.position, targets [j].transform.position);
-								if (currentDistance > largestDistance) {
-										largestDistance = currentDistance;
-								}
-						}
-				}
-				return largestDistance;
-		}
+
+        // 'AVG BOX' code is simply debug code that's useful when trying to figure out how the camera is working. It 
+        // is set to be where ever the camera is looking at. 
+        //avgBox.transform.position = new Vector3 ((avgDistance.x + lookXOffset), avgDistance.y, (avgDistance.z + lookZOffset));
+
+        // Run debug code, if its needed
+        
+        if (dubugText) 
+        {
+            OnGUI();
+        }
+                
+    }
+
+    // Gets the largest distance between any two players, and returns it
+    public float returnLargestDifference ()
+    {
+        currentDistance = 0.0f;
+        largestDistance = 0.0f;
+        for (int i = 0; i < targets.Length; i++) {
+            for (int j = 0; j < targets.Length; j++) {
+                currentDistance = Vector3.Distance (targets [i].transform.position, targets [j].transform.position);
+                if (currentDistance > largestDistance) {
+                    largestDistance = currentDistance;
+                }
+            }
+        }
+        return largestDistance;
+    }
 }
+
+// This is the old code used in the previous version of the camera. Its kept in case we need to refer back on our
+// previous methods for new ideas, or alterations to the current code. 
+
+/* OLD CODE
+ * targets = GameObject.FindGameObjectsWithTag ("Player");
+        if (!GameObject.FindWithTag ("Player")) {
+            return;
+        }
+        Vector3 sum = new Vector3 (0, 0, 0);
+        for (int i = 0; i < targets.Length; i++) {
+            sum += targets [i].transform.position;
+        }
+
+        avgDistance = sum / targets.Length;
+
+        float largestDifference = returnLargestDifference ();
+
+        distance = Mathf.Lerp (height, largestDifference, 0.5f);
+
+        if (largestDifference < 8) {
+            distance = 8;
+            Vector3 temp = new Vector3 (theCamera.transform.position.x, 10, theCamera.transform.position.z);
+            theCamera.transform.position = temp;
+        }
+
+        if (largestDifference > 8) {
+            distance = 8;
+        }
+        */
