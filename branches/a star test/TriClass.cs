59d2c0eddb1f022bb0a818f2078fe6d9e6f03c77
jjackson <jjackson@47a60c44-a02a-4305-9b0b-3482a82a218c>
2014-10-01 17:36:42 +0000

    Committing an A* project for investigation. We might be able to fix it and use it for the game, depending on some factors.
    - James

---
+++ branches/a star test/TriClass.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TriClass: IComparer
+{
+	public int id;
+	public Vector3[] faceVertexes;
+	public TriClass[] neighbors;
+	public int numOfNeighbors;
+	public Vector3 centerPoint;
+	public TriClass(int idNum,Vector3[] verts)
+	{
+		id = idNum;
+		faceVertexes = verts;
+        neighbors = new TriClass[3];
+	}
+	
+
+	public int CompareTo(object a)
+	{
+		TriClass that = (TriClass)a;
+		if (that.faceVertexes [0] == faceVertexes [0] && that.faceVertexes [1] == faceVertexes [1] && that.faceVertexes [2] == faceVertexes [2] ||
+			that.faceVertexes [1] == faceVertexes [0] && that.faceVertexes [2] == faceVertexes [1] && that.faceVertexes [0] == faceVertexes [2] ||
+			that.faceVertexes [2] == faceVertexes [0] && that.faceVertexes [0] == faceVertexes [1] && that.faceVertexes [1] == faceVertexes [2]) 
+		{
+			return 0;
+		}
+		return id.CompareTo (that.id);
+	}
+
+	public int Compare(object a, object b)
+	{
+		TriClass curr = (TriClass)a;
+		TriClass that = (TriClass)b;
+
+		if (that.faceVertexes [0] == curr.faceVertexes [0] && that.faceVertexes [1] == curr.faceVertexes [1] && that.faceVertexes [2] == curr.faceVertexes [2] ||
+		    that.faceVertexes [1] == curr.faceVertexes [0] && that.faceVertexes [2] == curr.faceVertexes [1] && that.faceVertexes [0] == curr.faceVertexes [2] ||
+		    that.faceVertexes [2] == curr.faceVertexes [0] && that.faceVertexes [0] == curr.faceVertexes [1] && that.faceVertexes [1] == curr.faceVertexes [2]) 
+		{
+			return 0;
+		}
+		return curr.id.CompareTo (that.id);
+	}
+
+}
---
+++ branches/a star test/onClick.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onClick : MonoBehaviour {
+	// 'Moving and Object towards a raycast', re-written in C4
+	public float targetRadius = 1;
+	public int speed = 1;
+	private Vector3 target;
+
+
+	// Use this for initialization
+	void Start () {
+		target = transform.position;
+	}
+	
+	// Update is called once per frame
+	public Vector3 Update () {
+		target.y = transform.position.y;
+		if(Input.GetButtonDown ("Fire1")){
+			//Debug.Log("TEST");
+			RaycastHit hit;
+			Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(mRay, out hit)){
+				target = hit.point;
+				//Debug.Log("TEST2");
+			}
+		}
+	// Maybe one day when im a Big Boy, ill LERP instead
+		if (Vector3.Distance (transform.position, target) > targetRadius) {
+			transform.LookAt (target);
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+		//Debug.Log (target);
+		return target;
+	}
+
+}
