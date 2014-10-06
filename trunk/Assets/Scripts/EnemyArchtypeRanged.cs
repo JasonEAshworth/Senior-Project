090e861d3518ae0c7efab6b64a30a79b7c036d13
jwilson <jwilson@47a60c44-a02a-4305-9b0b-3482a82a218c>
2014-10-06 17:04:47 +0000

    Commited ranged ai, uses various ranged checks to chase and avoid.

---
+++ trunk/Assets/Scripts/EnemyArchtypeRanged.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyArchtypeRanged : EnemyBase {
+	/* The Ranged AI Class Follows the following rule Set
+	 * 1. Get in Range to call Fire() Based on Distance to the player
+	 * 2. If Enemy is in Your "Radius" Then you Move Away.
+	 * 3. Otherwise Fire() until Dead
+	 */	
+	public Transform target;
+	private Transform mTransform;
+
+	public float rotationSpeed = 3f;
+	public float eRange = 10f;
+	public float pDistance;
+	public float attackDistance = 5f;
+	public float giveUpThreshold = 15f;
+	public float attackRate = 1.5f;
+	public GameObject player;
+	public float moveSpeed = 1;
+
+	// Behavior / Rates
+	private bool chasing = false;
+	private float attackTime = Time.time;
+
+	// Use this for initialization
+	void Awake() 
+	{
+		mTransform = transform;
+	}
+	// Update is called once per frame
+	void Update() 
+	{
+		player = findClosestPlayerInRange (eRange);
+		target = player.transform;
+		pDistance = (target.position - mTransform.position).magnitude;				
+	
+		if (chasing) 
+		{
+			mTransform.rotation = Quaternion.Slerp (mTransform.rotation, Quaternion.LookRotation(target.position - mTransform.position), rotationSpeed*Time.deltaTime);
+			mTransform.position += mTransform.forward * moveSpeed * Time.deltaTime;
+
+			if(pDistance > giveUpThreshold)
+			{
+				chasing = false;
+			}
+
+			if(pDistance < eRange && Time.time > attackTime)
+			{
+				attackTime = Time.time + attackRate;
+			}
+		}
+		else
+		{
+			if(pDistance < eRange)
+			{
+				chasing = true;
+			}
+		}
+	}
+}
