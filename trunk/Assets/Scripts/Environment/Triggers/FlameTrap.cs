493846adefc239fd1584ce527978b9cbc03b187e
cschuette <cschuette@47a60c44-a02a-4305-9b0b-3482a82a218c>
2014-10-27 16:08:55 +0000

    Timed flame trap using yield and waitforseconds statements

---
+++ trunk/Assets/Scripts/Environment/Triggers/FlameTrap.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlameTrap : MonoBehaviour {
+
+	private bool isFiring = false;
+	
+	void Update()
+	{
+		if(!isFiring)
+		{
+			StartCoroutine(Fireball());
+		}
+	}
+	
+	IEnumerator Fireball()
+	{
+		isFiring = true;
+		yield return StartCoroutine(Wait());
+
+		ParticleSystem p = gameObject.GetComponent<ParticleSystem> ();
+		Debug.Log (p);
+		p.Play ();
+
+		isFiring = false;
+	}
+	
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds (3.0f);
+	}
+}
